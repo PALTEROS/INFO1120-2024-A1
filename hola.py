c12daf5d68590f15ae0a29e8806c64f09b8b4f58
squinenao <squinenao2024@alu.uct.cl>
2024-05-29 15:49:01 -0400

    cambio de prueba

--- hola.py
+++ hola.py
@@ -1,6 +1,19 @@
-G="HOLA"
-s="hola x2"
-print('HOLA MUNDO SJSJSJAKJDJFSDHOAOUFBOI' )
+import random
 
-print(1+1)
-input("hola")
+def generar_chiste():
+    chistes = [
+        "¿Por qué los pájaros no usan Facebook? Porque ya tienen Twitter.",
+        "¿Qué hace una abeja en el gimnasio? ¡Zum-ba!",
+        "¿Por qué los esqueletos no pelean entre ellos? Porque no tienen agallas.",
+        "¿Cómo se dice pañuelo en japonés? Saka-moko.",
+        "¿Por qué los pingüinos siempre llevan corbata? Porque les queda muy bien con su esmoquin.",
+        "¿Qué hace una abeja en el gimnasio? ¡Zum-ba!",
+        "¿Por qué los esqueletos no pelean entre ellos? Porque no tienen agallas.",
+        "¿Cómo se dice pañuelo en japonés? Saka-moko.",
+        "¿Por qué los pingüinos siempre llevan corbata? Porque les queda muy bien con su esmoquin."
+    ]
+
+    return random.choice(chistes)
+
+if __name__ == "__main__":
+    print(generar_chiste())
